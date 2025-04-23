5c2007ad80f37dd8474df5af25169d35d77d0722
Sungsoo Kim <sungsoo81@gmail.com>
2025-04-23 13:29:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
 # Streamlit 메인 앱
+import streamlit as st
+
+st.set_page_config(page_title="SmartSwing Tracker", layout="wide")
+
+st.title("📈 SmartSwing Tracker")
+st.markdown("이 앱은 미국 주식 자동 추천 및 전략 기반 트래킹 시스템입니다.")
